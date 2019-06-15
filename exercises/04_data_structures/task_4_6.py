f8a83e8960114e1d57e3b12b19ac19373d68d8ab
gusevroman <gusevroman@mail.ru>
2019-06-15 14:44:59 +0000

    task_4_6 second variant

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -13,7 +13,7 @@ Outbound Interface     FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
-
+'''
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
 ospf_keys = ['Protocol', 'Prefix', 'AD/Metric', 'Next-Hop', 'Last update', 'Outbound Interface']
 
@@ -29,3 +29,20 @@ print('{:23}{}'.format(ospf_keys[2]+':', ospf_route[2]))
 print('{:23}{}'.format(ospf_keys[3]+':', ospf_route[4]))
 print('{:23}{}'.format(ospf_keys[4]+':', ospf_route[5]))
 print('{:23}{}'.format(ospf_keys[5]+':', ospf_route[6]))
+'''
+
+ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = ospf_route.replace('[', '') # убираем лишние квадратные скобки
+ospf_route = ospf_route.replace(']', '') # убираем лишние квадратные скобки
+ospf_route = ospf_route.replace(',', '') # убираем лишние запятые
+f = ospf_route.split() # получился список
+
+ip_template = '''
+Protocol:              {}
+Prefix:                {}
+AD/Metric:             {}
+Next-Hop:              {}
+Last update:           {}
+Outbound Interface     {}
+'''
+print(ip_template.format('OSPF', f[1], f[2], f[4], f[5], f[6]))
