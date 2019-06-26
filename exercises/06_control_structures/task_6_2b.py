76a2eee2f1ad1c28863fb19da174034252c6e132
pyneng <pyneng.course@gmail.com>
2019-06-26 15:48:03 +0000

    Загрузила решения в ветку task_check

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -9,3 +9,36 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+input_ip_address = input('Input ip-address: ').split('.')
+
+ip_correct = False
+
+# Проверка правильности ввода ip-адреса
+while not ip_correct:
+    if len(input_ip_address) != 4:
+        print('Неправильный ip-адрес - не состоит из 4 элементов разделенных точкой')
+    if (len(input_ip_address)) == 4:
+        ip_address = [int(octet) for octet in input_ip_address if octet.isdigit() and int(octet) in range(256)]
+        if len(ip_address) == 4:
+            ip_correct = True
+        else:
+            print('Неправильный ip-адрес')
+            input_ip_address = input('Введите ip-address еще раз: ').split('.')
+    else:
+        input_ip_address = input('Введите ip-address еще раз: ').split('.')
+
+# Классификация ip-адреса
+print('-' * 50, end='')
+if ip_address[0] in range(1, 224):
+    result = 'unicast'
+elif ip_address[0] in range(224, 239):
+    result = 'multicast'
+elif ip_address == [255, 255, 255, 255]:
+    result = 'local broadcast'
+elif ip_address == [0, 0, 0, 0]:
+    result = 'unassigned'
+else:
+    result = 'unused'
+print()
+print(result)
