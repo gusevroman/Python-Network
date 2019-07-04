345169609237050649b81dd76000f428c65474c6
gusevroman <gusevroman@mail.ru>
2019-07-04 23:32:58 +0300

    task_11_2

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -15,3 +15,61 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+import subprocess
+
+
+def ping_ip(ip_address):
+    '''
+    The result of the function is True on success or False if ip-address is unreachable.
+    :param ip_address:
+    :return:
+    '''
+    reply = subprocess.run(['ping', '-c', '3', '-n', ip_address])
+    if reply.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def ping_ip_addresses(ip_addresses):
+    '''
+    The result of the function is a tuple with two lists of transmitted ip-addresses. One of them is a list
+    of received. The second list of losses.
+    :param ip_addresses:            # list of ip-addresses
+    :return: trans_ip_addresses     # tuple([received], [losses])
+    '''
+    trans_received = []
+    trans_losses = []
+    for ip_address in ip_addresses:
+        if ping_ip(ip_address):
+            trans_received.append(ip_address)
+        else:
+            trans_losses.append(ip_address)
+    result = (trans_received, trans_losses)
+    return result
+
+"""
+# v.1
+def ping_ip_addresses(ip_addresses):
+    '''
+    The result of the function is a tuple with two lists of transmitted ip-addresses. One of them is a list
+    of received. The second list of losses.
+    :param ip_addresses:            # list of ip-addresses
+    :return: trans_ip_addresses     # tuple([received], [losses])
+    '''
+    trans_received = []
+    trans_losses = []
+    for ip_address in ip_addresses:
+        reply = subprocess.run(['ping', '-c', '3', '-n', ip_address])
+        if reply.returncode == 0:
+            trans_received.append(ip_address)
+        else:
+            trans_losses.append(ip_address)
+    result = (trans_received, trans_losses)
+    return result
+"""
+
+list_of_ip = ['1.1.1.3', '8.8.8.8', '8.8.4.4', '8.8.7.1']
+
+print(ping_ip_addresses(list_of_ip))
+# print(ping_ip('8.8.8'))
