91535ade5b1e672d16455f1eefdd2411b24d954f
gusevroman <gusevroman@mail.ru>
2019-07-10 11:48:40 +0300

    task_15_2 done

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -30,6 +30,7 @@ def parse_sh_ip_int_br(dev_cfg):
     :param dev_cfg: file with output from command show ip int br
     :return: dev_status: Interface, IP-Address,  Status, Protocol - The list of tuples:
     '''
+    # v.1
     # regex = (r'^(\S+) +'
     #          # r'([\d.]+|unassigned) +'
     #          r'(\S+) +'
@@ -37,8 +38,7 @@ def parse_sh_ip_int_br(dev_cfg):
     #          r'(up|down|administratively down) +'
     #          r'(up|down)')
 
-    # regex = '(\S+) +(\S+) +.+ +(up|down) +(up|down)'
-
+    # v.2
     regex = '(\S+) +(\S+) +\w+ +\w+ +(up|down|administratively down) +(up|down)'
     with open(dev_cfg) as f:
         line = f.read()
@@ -49,4 +49,3 @@ def parse_sh_ip_int_br(dev_cfg):
     return dev_status
 
 dev_config = 'sh_ip_int_br.txt'
-parse_sh_ip_int_br(dev_config)
