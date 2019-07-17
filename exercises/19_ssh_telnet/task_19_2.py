d28cd62916bb5d5cbae5caaacac693b40641a439
gusevroman <gusevroman@mail.ru>
2019-07-17 18:18:41 +0000

    task_19_2 done

--- exercises/19_ssh_telnet/task_19_2.py
+++ exercises/19_ssh_telnet/task_19_2.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 19.2
 
 Создать функцию send_config_commands
 
-Функция подключается по SSH (с помощью netmiko) к устройству и выполняет перечень команд в конфигурационном режиме на основании переданных аргументов.
+Функция подключается по SSH (с помощью netmiko) к устройству и выполняет перечень команд в конфигурационном режиме
+на основании переданных аргументов.
 
 Параметры функции:
 * device - словарь с параметрами подключения к устройству
@@ -26,7 +27,8 @@ Out[8]: ['logging 10.255.255.1', 'logging buffered 20010', 'no logging console']
 In [9]: result = send_config_commands(r1, commands)
 
 In [10]: result
-Out[10]: 'config term\nEnter configuration commands, one per line.  End with CNTL/Z.\nR1(config)#logging 10.255.255.1\nR1(config)#logging buffered 20010\nR1(config)#no logging console\nR1(config)#end\nR1#'
+Out[10]: 'config term\nEnter configuration commands, one per line.  End with CNTL/Z.\nR1(config)#logging 10.255.255.1\n
+R1(config)#logging buffered 20010\nR1(config)#no logging console\nR1(config)#end\nR1#'
 
 In [11]: print(result)
 config term
@@ -39,8 +41,35 @@ R1#
 
 
 Скрипт должен отправлять команду command на все устройства из файла devices.yaml с помощью функции send_config_commands.
-'''
+"""
+import yaml
+from netmiko import ConnectHandler
+
 
 commands = [
     'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
 ]
+
+
+def send_config_commands(device, config_commands):
+    """
+    The Function connects to the device by ssh and runs command in enable
+    :param device:  dictionary with parameters of connections to device
+    :param config_commands: list of commands for run
+    :return: string with results of run command
+    """
+    print('\nconnection to device {} ...'.format(device['ip']))
+
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(config_commands)
+        print(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    with open('devices.yaml') as f:
+        data = yaml.safe_load(f)
+        for device in data:
+            send_config_commands(device, commands)
