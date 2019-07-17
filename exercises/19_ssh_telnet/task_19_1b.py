e129e974300a785f993419796853660b91d6c0ed
gusevroman <gusevroman@mail.ru>
2019-07-17 13:31:57 +0000

    task_19_1a - 19_1b done

--- exercises/19_ssh_telnet/task_19_1b.py
+++ exercises/19_ssh_telnet/task_19_1b.py
@@ -11,3 +11,37 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 '''
+import yaml
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import AuthenticationException, NetMikoTimeoutException
+
+
+command = 'sh ip int br'
+
+
+def send_show_command(device, command):
+    '''
+    Function connects to the device by ssh and runs command.
+    :param device: It's a dict with parameters of connection.
+    :param command:
+    :return: - String with command.
+    '''
+    print('\nconnection to device {} ...'.format(device['ip']))
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+    except AuthenticationException:
+        result = 'Authentication failure: unable to connect {} {}'.format(device['device_type'], device['ip'])
+    except NetMikoTimeoutException:
+        result = 'Connection to device timed-out: {} {}'.format(device['device_type'], device['ip'])
+
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    with open('devices.yaml') as f:
+        data = yaml.safe_load(f)
+        for device in data:
+            send_show_command(device, command)
