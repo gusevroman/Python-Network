f64e3107cd2cd62429a7729891a7589a4aa74779
gusevroman <gusevroman@mail.ru>
2019-07-17 18:41:32 +0000

    task_19_2a done

--- exercises/19_ssh_telnet/task_19_2a.py
+++ exercises/19_ssh_telnet/task_19_2a.py
@@ -21,3 +21,36 @@ In [15]:
 
 Скрипт должен отправлять список команд commands на все устройства из файла devices.yaml с помощью функции send_config_commands.
 '''
+import yaml
+from netmiko import ConnectHandler
+
+
+commands = [
+    'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
+]
+
+
+def send_config_commands(device, config_commands, verbose=True):
+    """
+    The Function connects to the device by ssh and runs command in enable
+    :param device:  dictionary with parameters of connections to device
+    :param config_commands: list of commands for run
+    :return: string with results of run command
+    """
+    if verbose == True:
+        print('\nconnection to device {} ...'.format(device['ip']))
+
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_config_set(config_commands)
+        if verbose == True:
+            print(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    with open('devices.yaml') as f:
+        data = yaml.safe_load(f)
+        for device in data:
+            send_config_commands(device, commands)
