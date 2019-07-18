dacb11b3bb5f92e2736212794f0ddf110ee6b185
pyneng <pyneng.course@gmail.com>
2019-07-18 17:20:31 +0000

    Проверила задания 17.2a 17.2b

--- exercises/17_serialization/task_17_2a.py
+++ exercises/17_serialization/task_17_2a.py
@@ -75,3 +75,28 @@ def generate_topology_from_cdp(list_of_files, save_to_filename=None):
 
 if __name__ == "__main__":
     pprint(generate_topology_from_cdp(sh_cdp_files, 'topology.yaml'))
+
+# Все отлично, только надо импортировать parse_sh_cdp_neighbors, не копировать
+
+# вариант решения
+
+import yaml
+import glob
+from task_17_2 import parse_sh_cdp_neighbors
+
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    topology = {}
+    for filename in list_of_files:
+        with open(filename) as f:
+            topology.update(parse_sh_cdp_neighbors(f.read()))
+    if save_to_filename:
+        with open(save_to_filename, 'w') as f_out:
+            yaml.dump(topology, f_out, default_flow_style=False)
+    return topology
+
+
+if __name__ == '__main__':
+    f_list = glob.glob('sh_cdp_n_*')
+    print(generate_topology_from_cdp(f_list, save_to_filename='topology.yaml'))
+
