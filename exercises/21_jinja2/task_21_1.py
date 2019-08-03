b68824d7d3289171050503329c8fe15a7280a5d6
gusevroman <gusevroman@mail.ru>
2019-08-03 14:23:07 +0000

    task_21_1 done

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -13,3 +13,34 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 '''
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import sys
+import os
+# sys.path.append('..')
+
+
+
+def generate_config(template, data_dict):
+    """
+    The function's result is generated string of configuration
+    :param template: path to file with template( ex. "/templates/for.txt")
+    :param data_dict: dictionary with values for template
+    :return: strings of configuration
+    """
+    router = data_dict
+    template_folder, file_name = os.path.split(template)
+    env = Environment(
+        loader=FileSystemLoader(template_folder),
+        trim_blocks=True,
+        lstrip_blocks=True)
+    template = env.get_template(file_name)
+    result = template.render(router)
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    router = yaml.safe_load(open('data_files/for.yml'))
+    generate_config('templates/for.txt', router)
