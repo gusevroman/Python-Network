657c22cdb93ae1028115f15d7a62ae487ff2c1a8
gusevroman <gusevroman@mail.ru>
2019-08-05 20:26:40 +0000

    task_22_3 done

--- exercises/22_textfsm/task_22_3.py
+++ exercises/22_textfsm/task_22_3.py
@@ -18,3 +18,50 @@
 
 Проверить работу функции на примере вывода команды sh ip int br.
 '''
+import textfsm
+from textfsm import clitable
+from pprint import pprint
+
+
+def parse_command_dynamic(command_output, attributes_dict, index_file='index', templ_path='templates'):
+    """
+    The function converts the result of the output of the command into a dictionary
+    :param command_output: output from command (string)
+    :param attributes_dict: dictionaries of attributes with key-value:
+         * 'Command': command
+         * 'Vendor': vendor
+    :param index_file: name of the file where the correspondence between commands and templates is stored.
+        The default value is "index"
+    :param templ_path: directory where templates is stored/ The default "templates"
+    :return: is list of dictionaries output from command (ex. task_22_1a):
+        * keys - name of the variables in templates TextFSM
+        * values - are parts of the output from command that correspond to variables
+    """
+    result = []
+    cli_table = clitable.CliTable(index_file, templ_path)
+    cli_table.ParseCmd(command_output, attributes_dict)
+    print('CLI Table output:\n', cli_table)
+
+    print('Formatted Table:\n', cli_table.FormattedTable())
+
+    data_rows = [list(row) for row in cli_table]
+    header = list(cli_table.header)
+
+    print('-' * 79)
+    print('List of lists:\n', header)
+    for row in data_rows:
+        print(row)
+    print('-' * 79)
+
+    for item in cli_table:
+        result_dict = dict(zip(header, item))
+        result.append(result_dict)
+
+    return result
+
+
+if __name__ == '__main__':
+    with open("output/sh_ip_int_br.txt") as f:
+        command_output = f.read()
+    attributes = {'Command': 'show ip int br', 'Vendor': 'cisco_ios'}
+    pprint(parse_command_dynamic(command_output, attributes))
