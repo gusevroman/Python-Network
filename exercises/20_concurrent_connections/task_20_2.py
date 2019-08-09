878df46966c897b57ae34a8cf10177b3571b6ff3
pyneng <pyneng.course@gmail.com>
2019-08-09 17:13:42 +0000

    Загрузила решения в ветку task_check

--- exercises/20_concurrent_connections/task_20_2.py
+++ exercises/20_concurrent_connections/task_20_2.py
@@ -33,3 +33,55 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 '''
+import yaml
+from netmiko import ConnectHandler
+from datetime import datetime
+import logging
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor
+
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+def send_show(device, command):
+    start_msg = '===> {} Connection: {}'
+    received_msg = '<=== {} Received:   {}'
+    ip = device['ip']
+    logging.info(start_msg.format(datetime.now().time(), ip))
+    with ConnectHandler(**device) as ssh:
+        ssh.enable()
+        host_name = ssh.find_prompt()
+        result = ssh.send_command(command)
+        logging.info(received_msg.format(datetime.now().time(), ip))
+        return host_name, command, result
+
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    """
+    The function sends command show to device in parallel threads. Then write results into file.
+    :param devices: list of dictionaries with parameters of connections to devices
+    :param command: command (ex. show)
+    :param filename: name of file where to write output of all commands
+    :param limit: maximum count parallel threads (default 3)
+    :return: The function hasn't result
+    """
+    with open(filename, 'w') as dest:
+        with ThreadPoolExecutor(max_workers=limit) as executor:
+            result = executor.map(send_show, devices, repeat(command))
+            for device, output in zip(devices, result):
+                host_name, command_send, command_output = output
+                dest.write(host_name + command_send+'\n')
+                dest.write(command_output + '\n')
+
+
+if __name__ == '__main__':
+    with open('devices.yaml') as src:
+        devices = yaml.safe_load(src)
+        start_time = datetime.now()
+        send_show_command_to_devices(devices, 'sh ip int br', 'devices_sh_br.txt', 3)
+        print('time of running command', datetime.now() - start_time)
--- exercises/20_concurrent_connections/task_20_3.py
+++ exercises/20_concurrent_connections/task_20_3.py
@@ -21,7 +21,7 @@ Interface                  IP-Address      OK? Method Status                Prot
 Ethernet0/0                192.168.100.1   YES NVRAM  up                    up
 Ethernet0/1                192.168.200.1   YES NVRAM  up                    up
 R2#sh arp
-Protocol  Address          Age (min)  Hardware Addr   Type   Interface
+Protocol  Address          Age (min)  Hardware Addr   Type   Intesend_show, rface
 Internet  192.168.100.1          76   aabb.cc00.6500  ARPA   Ethernet0/0
 Internet  192.168.100.2           -   aabb.cc00.6600  ARPA   Ethernet0/0
 Internet  192.168.100.3         173   aabb.cc00.6700  ARPA   Ethernet0/0
@@ -35,8 +35,55 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml и словаре commands
 '''
+import yaml
+
+from netmiko import ConnectHandler, NetMikoAuthenticationException
+from datetime import datetime
+import logging
+from itertools import repeat
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from task_20_2 import send_show, send_show_command_to_devices
+from pprint import pprint
+import time
+
 
 commands = {'192.168.100.1': 'sh ip int br',
             '192.168.100.2': 'sh arp',
             '192.168.100.3': 'sh ip int br'}
 
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+def send_command_to_devices(devices, commands_dict, filename, limit=3):
+    """
+    The function sends different commands show to device in parallel threads. Then write results into file.
+    :param devices: list of dictionaries with parameters of connection to device
+    :param commands_dict: dictionary, says which command send to which device
+    :param filename: name of file where write output of commands
+    :param limit: maximum of threads(default is 3)
+    :return: writes output into file
+    """
+
+    with open(filename, 'w') as dest:
+        with ThreadPoolExecutor(max_workers=limit) as executor:
+            future_list = []
+            for device in devices:
+                future = executor.submit(send_show, device, commands_dict[device['ip']])
+                future_list.append(future)
+            for f in as_completed(future_list):
+                result = f.result()
+                host_name, command_send, command_output = result
+                dest.write(host_name + command_send+'\n')
+                dest.write(command_output + '\n')
+
+
+if __name__ == '__main__':
+    with open('devices.yaml') as src:
+        devices_y = yaml.safe_load(src)
+        start_time = datetime.now()
+        send_command_to_devices(devices_y, commands, 'devices_show.txt', 3)
+        print('time of running command', datetime.now() - start_time)
