c5f49733fd73e1d2ffb683f93e33dc93d7f1f7fe
gusevroman <gusevroman@mail.ru>
2019-08-21 19:08:19 +0000

    task_27_1 done

--- exercises/27_oop_inheritance/task_27_1.py
+++ exercises/27_oop_inheritance/task_27_1.py
@@ -17,6 +17,8 @@ In [4]: r1.send_show_command('sh ip int br')
 Out[4]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
 
 '''
+from base_connect_class import BaseSSH
+
 
 device_params = {
     'device_type': 'cisco_ios',
@@ -26,3 +28,11 @@ device_params = {
     'secret': 'cisco'
 }
 
+
+class CiscoSSH(BaseSSH):
+    def __init__(self, disable_paging=True, **device_params):
+        super().__init__(**device_params)
+
+if __name__ == '__main__':
+    r1 = CiscoSSH(**device_params)
+    r1.send_show_command('sh ip int br')
