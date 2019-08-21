b88cd9c5e2113e77bda3b80f31e18558c8576bd8
gusevroman <gusevroman@mail.ru>
2019-08-21 09:18:26 +0000

    task_26_1 done

--- exercises/26_oop_special_methods/task_26_1.py
+++ exercises/26_oop_special_methods/task_26_1.py
@@ -58,6 +58,8 @@ Out[9]:
 In [10]: t2.topology
 Out[10]: {('R1', 'Eth0/4'): ('R7', 'Eth0/0'), ('R1', 'Eth0/6'): ('R9', 'Eth0/0')}
 '''
+from pprint import pprint
+
 
 topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
                     ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
@@ -71,4 +73,66 @@ topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
 
 topology_example2 = {('R1', 'Eth0/4'): ('R7', 'Eth0/0'),
                      ('R1', 'Eth0/6'): ('R9', 'Eth0/0')}
+
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __add__(self, other):
+        """
+        Addition of two instances of the Topology class
+        """
+        sum_instance = Topology(self.topology)
+        sum_instance.topology.update(other.topology)
+        return sum_instance
+
+    def _normalize(self, topology_dict):
+        topology = {}
+        for l_device, r_device in topology_dict.items():
+            if r_device not in topology:
+                topology[l_device] = r_device
+        return topology
+
+    def delete_link(self, link1, link2):
+        if (link1, link2) in self.topology.items():
+            del(self.topology[link1])
+        elif (link2, link1) in self.topology.items():
+            del(self.topology[link2])
+        else:
+            print('Такого соединения нет ')
+        # return self.topology
+
+    def delete_node(self, node):
+        ports_with_node = {}
+        for src, dst in self.topology.items():
+            if node not in src and node not in dst:
+                ports_with_node[src] = dst
+        if len(ports_with_node) == len(self.topology):
+            print('Такого устройства нет')
+        else:
+            self.topology = ports_with_node
+
+    def add_link(self, link_1, link_2):
+        if (link_1, link_2) in self.topology.items():
+            print('Такое соединение существует')
+        elif (link_2, link_1) in self.topology.items():
+            print('Такое соединение существует!')
+        elif link_1 in self.topology.keys() or link_2 in self.topology.keys():
+            print('Cоединение с одним из портов существует')
+        elif link_1 in self.topology.values() or link_2 in self.topology.values():
+            print('Cоединение с одним из портов существует!')
+        else:
+            self.topology[link_1] = link_2
+
+
+if __name__ == '__main__':
+    t1 = Topology(topology_example)
+    # pprint(t1.topology)
+    t2 = Topology(topology_example2)
+    # pprint(t2.topology)
+    t3 = t1 + t2
+    pprint(t3.topology)
+
+
 
