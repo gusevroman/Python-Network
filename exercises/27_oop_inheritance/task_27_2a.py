a9cf6bad1ac8430653404fc54cf674367f846d72
pyneng <pyneng.course@gmail.com>
2019-08-22 15:31:32 +0000

    Загрузила решения в ветку task_check

--- exercises/27_oop_inheritance/task_27_2a.py
+++ exercises/27_oop_inheritance/task_27_2a.py
@@ -10,7 +10,8 @@
 
 Метод ожидает как аргумент команду и вывод команды.
 Если в выводе не обнаружена ошибка, метод ничего не возвращает.
-Если в выводе найдена ошибка, метод генерирует исключение ErrorInCommand с сообщениеем о том какая ошибка была обнаружена, на каком устройстве и в какой команде.
+Если в выводе найдена ошибка, метод генерирует исключение ErrorInCommand с сообщениеем о том какая ошибка была
+обнаружена, на каком устройстве и в какой команде.
 
 Переписать метод send_command netmiko, добавив в него проверку на ошибки.
 
@@ -30,3 +31,36 @@ ErrorInCommand                            Traceback (most recent call last)
 ErrorInCommand: При выполнении команды "sh ip br" на устройстве 192.168.100.1 возникла ошибка "Invalid input detected at '^' marker."
 
 '''
+from netmiko.cisco.cisco_ios import CiscoIosBase
+
+
+device_params = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.100.1',
+    'username': 'cisco',
+    'password': 'cisco',
+    'secret': 'cisco'
+}
+
+
+class ErrorInCommand(Exception):
+    """Это исключение генерируется при ошибке в команде"""
+
+
+class MyNetmiko(CiscoIosBase):
+    def __init__(self, **device_params):
+        super().__init__(**device_params)
+        self.enable()
+
+    def _check_error_in_command(self, command_output):
+        if ('Invalid input detected' or 'Incomplete command' or 'Ambiguous command') in command_output:
+            raise ErrorInCommand("Возникла ошибка Invalid input detected")
+
+    def send_command(self, command, **kwargs):
+        output = super().send_command(command, **kwargs)
+        self._check_error_in_command(output)
+
+
+if __name__ == '__main__':
+    r1 = MyNetmiko(**device_params)
+    r1.send_command('sh ip int br1')
